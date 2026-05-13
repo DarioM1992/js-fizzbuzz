c4374baf4a8338360d0a19f8a9b028f48291bca3
DarioM1992 <dario21992@gmail.com>
2026-05-13 16:44:51 +0200

    trovato modo per multipli di 3

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 //parto con un ciclo per stampare i numeri da 1 a 100
 
-for (let n = 0; n < 100; n++)  console.log(n + 1);
+//for (let n = 0; n < 100; n++)  console.log(n + 1);
     
+//ora penso ad un modo per trovare i multipli di 3 e subito dopo i multipli di 5 
+
+for (let t = 3; t < 100; t++){
+    if (t % 3 === 0) { 
+    console.log("Fizz" ,t);
+    } 
+}
+
